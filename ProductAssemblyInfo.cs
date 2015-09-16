f510849e7978de84a6a724df4eec1c7e08c0302c
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2015-09-16 19:02:15 +0200

    Upping version again

--- ProductAssemblyInfo.cs
+++ ProductAssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.2")]
-[assembly: AssemblyFileVersion("0.0.2")]
+[assembly: AssemblyVersion("0.0.3")]
+[assembly: AssemblyFileVersion("0.0.3")]
 [assembly: AssemblyProduct("CSharp GeckoBoard Push")]
 [assembly: AssemblyCompany("Mattias Nordqvist")]
 [assembly: AssemblyCopyright("Copyright © Mattias Nordqvist")]
